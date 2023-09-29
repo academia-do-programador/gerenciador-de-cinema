1f64e57c1368c175f90b975faaeeefb34fed22d2
Tiago Santini <tiagosantiniathayde@gmail.com>
2023-09-29 14:21:32 -0300

    add: componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { ListaFilmesComponent } from './shared/lista-filmes/lista-filmes.component';
 import { CardFilmeComponent } from './shared/card-filme/card-filme.component';
 import { DetalhesFilmeComponent } from './views/detalhes-filme/detalhes-filme.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ListaFilmesComponent, CardFilmeComponent, DetalhesFilmeComponent],
+  declarations: [AppComponent, HomeComponent, ListaFilmesComponent, CardFilmeComponent, DetalhesFilmeComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
